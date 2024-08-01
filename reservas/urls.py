b603958ca0457b06368842cb3318f96edd261574
Endrio-Korb <alex.korb@uniasselvi.com.br>
2024-07-31 21:16:05 -0300

    alterado url de cancelar e editar reservas

--- reservas/urls.py
+++ reservas/urls.py
@@ -8,8 +8,8 @@ app_name = "reservas"
 
 urlpatterns = [
     path('reservas/', ReservarLabs.as_view(), name='reserva_form'),    
-    re_path('(?P<id>[0-9]+)/cancelar', cancelar_form, name='cancelar_form'),
-    re_path('(?P<id>[0-9])/cancelar_reserva', cancelar_reserva, name="cancelar_reserva"),
+    re_path('<int:id>/cancelar', cancelar_form, name='cancelar_form'),
+    re_path('<int:id>/cancelar_reserva', cancelar_reserva, name="cancelar_reserva"),
 
     
     path('', registrarReservarLaboratorio, name='registrar_reserva'),
@@ -17,7 +17,7 @@ urlpatterns = [
     path('editar_modules/', editar_modules, name='editar_modules'),
 
     #path('<int:pk>/editar', Editar.as_view(), name='editar'),
-    re_path('^(?P<id>[0-9]{1})/editar_form', editar_form, name='editar_form'),
-    re_path('(?P<id>[0-9]{1})/editar', editar, name='editar'),
+    re_path('<int:id>/editar_form', editar_form, name='editar_form'),
+    re_path('<int:id>/editar', editar, name='editar'),
 ]
 
