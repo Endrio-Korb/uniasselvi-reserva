425be3efc57c9532e73fd8c60d15f2bdcae5b9bd
Endrio-Korb <alexkorb95@gmail.com>
2024-11-08 13:28:11 -0300

    corrigido link e mensagem

--- usuarios/utils.py
+++ usuarios/utils.py
@@ -5,13 +5,12 @@ def enviar_email(pre_registro):
         Você recebeu esse e-mail pois você ou alguém o cadastrou na escola de cursos. Caso queira confirmar o cadastro, clique no link a seguir.
         Caso não tenha sido você, apenas ignore esse e-mail.
 
-        https://uniasselvireserva.com.br//auth/confirmacao?id={pre_registro.uuid}
+        https://uniasselvireserva.com.br/auth/confirmacao?id={pre_registro.uuid}
     
     """
 
     send_mail(
-        "Bem-vindo à escola de cursos",
+        "Bem-vindo à Uniasselvi",
         mensagem_email,
-        "admin@localhost",
         [pre_registro.email]
     )
